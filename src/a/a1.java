7fada1687c815682892b13e549514de25dc51bf9
Ashmi Minjrola <ashmi.minjrola@gmail.com>
2023-05-23 08:38:41 +0530

    Conflict resolve upadate in string

--- src/a/a1.java
+++ src/a/a1.java
@@ -3,11 +3,11 @@ package a;
 public class a1 {
 
 	public static void main(String[] args) {
+
 		System.out.println("a package class a1");
-		System.out.println("new update 1 a1");
+		System.out.println("new update 1 b1");
+		System.out.println("conflict resolve..");
 		System.out.println("new update 2 a1");
-		System.out.println("new update 3 a1");
-		System.out.println("new update 4 a1");
 	}
 
 }
